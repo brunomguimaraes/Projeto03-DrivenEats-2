b71446f4fbc44cc1497798f24aed44121b3d63e0
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-07-26 10:39:35 -0300

    Project finished

--- scripts.js
+++ scripts.js
@@ -7,8 +7,48 @@ let desertPrice;
 let username;
 let address;
 
-function selectFood (option) {
+function calculatePrice () {
+    foodPrice = Number(foodPrice.replace("R$ ", "").replace(",", "."));
+    drinkPrice = Number(drinkPrice.replace("R$ ", "").replace(",", "."));
+    desertPrice = Number(desertPrice.replace("R$ ", "").replace(",", "."));
+    total = foodPrice + drinkPrice + desertPrice;
+}
 
+function displayOrder () {
+    const background = document.querySelector(".blur-background");
+    background.classList.remove("hide")
+    const panel = document.querySelector(".confirm-order");
+    panel.classList.remove("hide")
+
+    const order = document.querySelector(".order");
+    order.innerHTML = order.innerHTML
+        .replace("{Food}", foodSelected.querySelector(".name").innerText)
+        .replace("{Drink}", drinkSelected.querySelector(".name").innerText)
+        .replace("{Desert}", desertSelected.querySelector(".name").innerText)
+        .replace("{FoodPrice}", foodSelected.querySelector(".price").innerText.replace("R$ ", ""))
+        .replace("{DrinkPrice}", drinkSelected.querySelector(".price").innerText.replace("R$ ", ""))
+        .replace("{DesertPrice}", desertSelected.querySelector(".price").innerText.replace("R$ ", ""))
+        .replace("{TotalPrice}", String(total.toFixed(2)).replace(".", ","));
+}
+
+function order () {
+    username = prompt("Digite seu nome:");
+    address = prompt("Digite seu endereço:");
+    calculatePrice();
+    displayOrder();  
+}
+
+function completeOrder () {
+    if (foodSelected !== undefined && drinkSelected !== undefined && desertSelected !== undefined) {
+        const orderButton = document.querySelector(".orderButton");
+        orderButton.innerHTML = "Fechar pedido";
+        orderButton.classList.remove("gray");
+        orderButton.classList.add("orderSelected");
+        orderButton.addEventListener("click", order);
+    }
+}
+
+function selectFood (option) {
     const selectedbutton = document.querySelector(".food .selected");
     if (selectedbutton !== null) {
         selectedbutton.classList.remove("selected");
@@ -20,7 +60,6 @@ function selectFood (option) {
 }
 
 function selectDrink (option) {
-
     const selectedbutton = document.querySelector(".drink .selected");
     if (selectedbutton !== null) {
         selectedbutton.classList.remove("selected");
@@ -32,7 +71,6 @@ function selectDrink (option) {
 }
 
 function selectDesert (option) {
-
     const selectedbutton = document.querySelector(".desert .selected");
     if (selectedbutton !== null) {
         selectedbutton.classList.remove("selected");
@@ -43,50 +81,8 @@ function selectDesert (option) {
     completeOrder();
 }
 
-function completeOrder () {
-
-    if (foodSelected !== undefined && drinkSelected !== undefined && desertSelected !== undefined) {
-        const orderButton = document.querySelector(".orderButton");
-
-        orderButton.innerHTML = "Fechar pedido";
-        orderButton.classList.remove("gray");
-        orderButton.classList.add("orderSelected");
-
-        foodPrice = Number(foodPrice.replace("R$ ", "").replace(",", "."));
-        drinkPrice = Number(drinkPrice.replace("R$ ", "").replace(",", "."));
-        desertPrice = Number(desertPrice.replace("R$ ", "").replace(",", "."));
-
-        total = foodPrice + drinkPrice + desertPrice;
-    }
-}
-
 function Whatsapp () {
-
     let message = `Olá, gostaria de fazer o pedido:\n- Prato: ${foodSelected.querySelector(".name").innerText}\n- Bebida: ${drinkSelected.querySelector(".name").innerText}\n- Sobremesa: ${desertSelected.querySelector(".name").innerText}\nTotal: R$ ${total.toFixed(2)}\n\nNome: ${username}\nEndereço: ${address}`;
     let link = "https://wa.me/5519981266625?text=" + encodeURIComponent(message);
-
     window.location = link;
-}
-
-
-function Order () {
-
-    username = prompt("Digite seu nome:");
-    address = prompt("Digite seu endereço:");
-
-    const background = document.querySelector(".blur-background");
-    background.classList.remove("hide")
-    const panel = document.querySelector(".confirm-order");
-    panel.classList.remove("hide")
-
-    const order = document.querySelector(".order");
-
-    order.innerHTML = order.innerHTML
-        .replace("{Food}", foodSelected.querySelector(".name").innerText)
-        .replace("{Drink}", drinkSelected.querySelector(".name").innerText)
-        .replace("{Desert}", desertSelected.querySelector(".name").innerText)
-        .replace("{FoodPrice}", foodSelected.querySelector(".price").innerText.replace("R$ ", ""))
-        .replace("{DrinkPrice}", drinkSelected.querySelector(".price").innerText.replace("R$ ", ""))
-        .replace("{DesertPrice}", desertSelected.querySelector(".price").innerText.replace("R$ ", ""))
-        .replace("{TotalPrice}", String(total.toFixed(2)).replace(".", ","));
 }
